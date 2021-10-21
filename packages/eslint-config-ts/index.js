eab10ba7d47a8c7c5eb8a1f091066a4f2509b7e7
Mario34 <zhl.mario34@outlook.com>
2021-10-21 23:04:43 +0800

    fix: 更新ts预设

--- packages/eslint-config-ts/index.js
+++ packages/eslint-config-ts/index.js
@@ -1,10 +1,8 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
-  parserOptions: { sourceType: 'module', ecmaVersion: 2020, project: ['tsconfig.json'] },
   plugins: ['@typescript-eslint'],
   extends: [
     '@mario34/eslint-config-base',
-    '@mario34/eslint-config-base/rules/errors',
     'plugin:@typescript-eslint/recommended',
     ...[
       // './rules/all.js',
